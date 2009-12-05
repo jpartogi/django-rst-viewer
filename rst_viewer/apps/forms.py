bd5dba2ee54de69efa27823fb80fce20851900db
Joshua Partogi <jpartogi@r1.(none)>
2009-12-05 13:01:06 +1100

    make textarea larger

--- rst_viewer/apps/forms.py
+++ rst_viewer/apps/forms.py
@@ -2,4 +2,4 @@ from django import forms
 from django.forms.widgets import Textarea
 
 class ViewerForm(forms.Form):
-    description = forms.CharField(widget=Textarea, required=False)
+    description = forms.CharField(widget=Textarea(attrs={'cols': 90, 'rows': 15}), required=False)
